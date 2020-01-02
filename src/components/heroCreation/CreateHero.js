9bc8d1086fe02cde9277f44507fde7161c012339
StiX1919 <stix1919@hotmail.com>
2020-01-02 14:48:09 -0700

    Small styling updates

--- src/components/heroCreation/CreateHero.js
+++ src/components/heroCreation/CreateHero.js
@@ -36,28 +36,30 @@ function App(props) {
     console.log(props)
     return (
         <div>
-            <div className='info'>
-                <h1>Account Info</h1>
-                <h2>Real Name: {state.name}</h2>
-                <input value={state.name} placeholder='enter name' onChange={(e) => setState({...state, name: e.target.value})}/>
-                <h2>Age: {state.age}</h2>
-                <input type='date' onChange={(e) => setState({...state, age: e.target.value})}/>
-            </div>
-            <h3>Hero Name: {props.hero.name}</h3>
-                <input value={props.hero.name} placeholder='enter name' onChange={(e) => props.updateName(e.target.value)}/>
-            <h3>Stat Points: {props.bonusStats}</h3>
-            <div className='stats'>
-                {statsEntries}
-                <div>
-                    <h3>HP: {Math.floor(props.hero.stats.power + props.hero.stats.endurance + props.hero.luck)}</h3>
-                    <h3>MP: {Math.floor((props.hero.luck / 500) * (props.hero.stats.wisdom + props.hero.stats.intelligence))}</h3>
-                    <h3>SP: {Math.floor((props.hero.stats.agility + props.hero.stats.endurance) * (props.hero.luck / 500))}</h3>
+            <h1>Account Info</h1>
+            <div id='inputs'>
+                <div className='info'>
+                    <h2>Real Name: {state.name}</h2>
+                    <input value={state.name} placeholder='enter name' onChange={(e) => setState({...state, name: e.target.value})}/>
+                    <h2>Age: {state.age}</h2>
+                    <input type='date' onChange={(e) => setState({...state, age: e.target.value})}/>
+                    <h3>Hero Name: {props.hero.name}</h3>
+                    <input value={props.hero.name} placeholder='enter name' onChange={(e) => props.updateName(e.target.value)}/>
+                    <h3>Stat Points: {props.bonusStats}</h3>
                 </div>
+                <div className='stats'>
+                    {statsEntries}
+                    <div>
+                        <h3>HP: {Math.floor(props.hero.stats.power + props.hero.stats.endurance + props.hero.luck)}</h3>
+                        <h3>MP: {Math.floor((props.hero.luck / 500) * (props.hero.stats.wisdom + props.hero.stats.intelligence))}</h3>
+                        <h3>SP: {Math.floor((props.hero.stats.agility + props.hero.stats.endurance) * (props.hero.luck / 500))}</h3>
+                    </div>
+                </div>
+                <Link to='/Map'>
+                    <button disabled={!props.bonusStats && state.age && state.name ? false : true} onClick={handleSubmit}>Submit Hero</button>
+                </Link>
+            
             </div>
-            <Link to='/Map'>
-                <button disabled={!props.bonusStats && state.age && state.name ? false : true} onClick={handleSubmit}>Submit Hero</button>
-            </Link>
-
         </div>
     );
 }
